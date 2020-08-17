c50eece1285efcbd609ea9347a582e212667a55a
Nelson Jr <juniordell123@gmail.com>
2020-08-17 00:33:34 -0300

    add and learn before and after functions and learn test.only

--- sum.test.js
+++ sum.test.js
@@ -46,6 +46,23 @@ test('there is no I in team  ', () => {
     expect('team').not.toMatch(/I/)
 });
 
-test('there is a stop on Christoph', () => {
-    expect('Christoph').toMatch(/stop/)
+// test.only('there is no I in team  ', () => {
+//     expect('team').not.toMatch(/I/)
+// });
+
+test('there is a stop on Christopher', () => {
+    expect('Christopher').toMatch(/stop/)
 })
+
+beforeAll(() => console.log('1 - beforeAll'));
+afterAll(() => console.log('1 - afterAll'));
+beforeEach(() => console.log('1 - beforeEach'));
+afterEach(() => console.log('1 - afterEach'));
+test('', () => console.log('1 - test'));
+describe('Scoped / Nested block', () => {
+  beforeAll(() => console.log('2 - beforeAll'));
+  afterAll(() => console.log('2 - afterAll'));
+  beforeEach(() => console.log('2 - beforeEach'));
+  afterEach(() => console.log('2 - afterEach'));
+  test('', () => console.log('2 - test'));
+});
